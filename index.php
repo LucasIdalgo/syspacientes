69fb70c3fc05ecc94ec9cddcf9879adb714ea79c
Lucas Idalgo <lucasidalgodossantos@gmail.com>
2020-08-26 20:49:24 -0300

    Criando $_GET

--- index.php
+++ index.php
@@ -1,19 +1,22 @@
 <?php
     //echo "Boa noite"
-    $con=mysqli_connect(
-        "localhost",
-        "root",
-        "123456",
-        "syspacientes"
-    );
+    //$con=mysqli_connect(
+    //    "localhost",
+    //    "root",
+    //    "123456",
+    //    "syspacientes"
+    //);
 
-    if(!$con){
-        echo("Error: ".php_eol);
-        exit;
-    }
-    echo "Conectou!!";
-    echo "<br>";
-    echo "Informações do host: ".mysql_get_host_info($con);
+    //if(!$con){
+    //    echo("Error: ".php_eol);
+    //    exit;
+    //}
+    //echo "Conectou!!";
+    //echo "<br>";
+    //echo "Informações do host: ".mysqli_get_host_info($con);
 
-    mysqli_close($con);
+    //mysqli_close($con);
+
+    $nome=$_GET["nome"];
+    echo "O nome é: ".$nome;
 ?>
