3551f05e107cee488abfe23896e4b68ec4437c0a
Lucas Idalgo <lucasidalgodossantos@gmail.com>
2020-08-26 22:37:11 -0300

    Correção

--- index.php
+++ index.php
@@ -17,8 +17,7 @@
 
     //mysqli_close($con);
     $onload="";
-    $erro="";
-    $erro=$_GET["erro"];
+    $erro=$_GET["erro"]="";
     if(strlen($erro)>0){
         $onload="onload='alert(\"$erro\")'";
     }
