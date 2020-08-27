84789dfa0576caefcff6939617ba509b85665dc1
Lucas Idalgo <lucasidalgodossantos@gmail.com>
2020-08-26 21:43:17 -0300

    Correção

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
     //mysqli_close($con);
 ?>
 <!DOCTYPE html>
-<html lang="pt-vr">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -29,7 +29,7 @@
     <link href="css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
-    <link href="login.css" rel="stylesheet">
+    <link href="css/login.css" rel="stylesheet">
 </head>
 <body class="text-center">
     <form class="form-signin">
