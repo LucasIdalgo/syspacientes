4f99dfe183732eaff1b2cb0e7e7f30486092693a
Lucas Idalgo <lucasidalgodossantos@gmail.com>
2020-08-26 22:54:05 -0300

    Correções

--- index.php
+++ index.php
@@ -1,70 +1,70 @@
 <?php
-    //echo "Boa noite"
-    //$con=mysqli_connect(
-    //    "localhost",
-    //    "root",
-    //    "123456",
-    //    "syspacientes"
-    //);
-
-    //if(!$con){
-    //    echo("Error: ".php_eol);
-    //    exit;
-    //}
-    //echo "Conectou!!";
-    //echo "<br>";
-    //echo "Informações do host: ".mysqli_get_host_info($con);
-
-    //mysqli_close($con);
-    $onload="";
-    $erro=$_GET['erro'];
-    if(strlen($erro)>0){
-        $onload="onload='alert(\"$erro\")'";
-    }
-?>
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
+   //echo "boa noite!";
+   /*
+   $con = mysqli_connect(
+      "localhost",
+      "root",
+      "123456",
+      "syspacientes");
+   if(!$con){
+      echo "Error ".PHP_EOL;
+      exit;
+   }
+   echo "Conectou!!!";
+   echo "<br>";
+   echo "Informações do host: " . 
+      mysqli_get_host_info($con);
+   mysqli_close($con);  
+   */
+   $onload = "";
+   $erro   = $_GET['erro'];
+   if(strlen($erro) > 0){
+      $onload = "onload='alert(\"$erro\")'";
+   }
+ ?>
+ <!DOCTYPE html>
+ <html lang="pt-br">
+ <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="img/favicon/favicon2.ico">
     <title>SysPacientes</title>
-
+    <link rel="icon" href="img/favicon/favicon2.ico">
     <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
     <link href="css/login.css" rel="stylesheet">
     <script type="text/javascript">
-        function validaTela(){
-            var resposta=true;
-            var senha=document.getElementById("inputPassword").value;
-            if(senha.length==0){
-                alert("Digite sua senha");
-                resposta=false;
-            }else if(senha.length>6){
-                alert("Senha maior que 6 caracteres");
-                resposta=false;
-            }
-            return resposta;
-        }
+      function validaTela(){
+         var resposta = true;
+         var senha = document.getElementById("inputPassword").value;
+         if(senha.length == 0){
+            alert('Digite sua senha');
+            resposta = false;
+         }else if(senha.length > 6){
+            alert('Senha maior que 6 caracteres');
+            resposta = false;
+         }
+         return resposta;
+      }
     </script>
-</head>
-<body class="text-center" <?php echo $onload?>>
+ </head>
+ <body class="text-center" <?php echo $onload; ?>>
     <form class="form-signin"
-    method="post"
-    action="redirect.php"
-    >   
-    <!--onsubmit="return validaTela()"-->     
-      <img class="mb-4" src="img/login.png" alt="" width="72" height="72">
+          method="post"
+          action="redirect.php"
+          >
+          <!--onsubmit="return validaTela()"-->
+          
+      <img class="mb-4" src="img/login.png" alt="" width="80" height="80">
       <h1 class="h3 mb-3 font-weight-normal">Entre em sua conta</h1>
       <label for="inputEmail" class="sr-only">Email</label>
       <input type="email" id="inputEmail" name="inputEmail" class="form-control" placeholder="Email" required autofocus>
       <label for="inputPassword" class="sr-only">Senha</label>
-      <input type="password" id="inputPassword" name="inputPassword" class="form-control" placeholder="Password" required>
+      <input type="password" id="inputPassword" name="inputPassword" class="form-control" placeholder="Senha" required>
       
       <button class="btn btn-lg btn-primary btn-block" type="submit">Entrar</button>
-      <p class="mt-5 mb-3 text-muted">Lucas Idalgo &copy; 2018-2020</p>
+      <p class="mt-5 mb-3 text-muted">Lucas Idalgo &copy; 2018-2021</p>
     </form>
   </body>
-</html>
+ </html>
--- redirect.php
+++ redirect.php
@@ -1,23 +1,36 @@
 <?php
-    $email=$_POST["inputEmail"];
-    $senha=$_POST["inputPassword"];
-    $validou=true;
-    $erro="";
+   require_once('variaveis.php');
+   require_once('conexao.php');
 
-    //validar senha
-    if(strlen($senha)<6){
-        $erro= "Senha menor que 6 caracteres";
-        $validou=false;
-    }else if(strlen($senha)>6){
-        $erro="Senha maior que 6 caracteres";
-        $validou=false;
-    }
+   $email      = $_POST["inputEmail"];
+   $senha      = $_POST["inputPassword"];
+   $validou    = false;
+   $erro       = "Nenhuma credencial encontrada!";
+   $id_usuario = 0;
 
-    //exibir ou retornar:
-    if($validou){
-        echo "<hr>Email: ".$email."<br>Senha: ".$senha;
-    }else{
-        header("location:index.php?erro=$erro");
-    }    
+   //validar login
+   $sql = "SELECT id, email, senha FROM usuarios WHERE email = '$email'";
+   $resp = mysqli_query($conexao_bd, $sql);
+   if($rows=mysqli_fetch_row($resp)){      
+      if($senha == $rows[2]){
+         $erro       = "";
+         $validou    = true;
+         $id_usuario = $rows[0];
+      }else{
+         $erro    = "Credenciais inválidas!";
+         $validou = false;
+      }
+   }
+   mysqli_close($conexao_bd);
+
+   //exibir ou retornar:
+   if($validou){
+      echo "<hr>";
+      echo "E-mail: " . $email."<br>";
+      echo "Senha: ".$senha;
+      header("location:admin.php?id_usuario=$id_usuario");
+   }else{
+      header("location:index.php?erro=$erro");
+   }
 
 ?>
